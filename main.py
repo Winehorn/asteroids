88c7d21587e22159b25d07041ab08807f58f9c67
Winehorn <22160065+Winehorn@users.noreply.github.com>
2025-01-15 21:55:37 +0100

    Add player

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 import constants
+import player
 
 
 def main():
@@ -10,11 +11,13 @@ def main():
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player_obj = player.Player(constants.SCREEN_WIDTH/2, constants.SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player_obj.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
